6f4d6c46be57c0ad3eb3d172229dd52a59f72ffc
Tabita Sadiq <tfadiq@students.mchenry.edu>
2024-03-07 13:14:56 -0600

    Initial Commit

---
+++ saveToTextFile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  saveToTextFile
+//
+//  Created by Tabita Sadiq on 3/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
